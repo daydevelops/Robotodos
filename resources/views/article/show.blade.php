366004d9ee66e4c80589a79c319c8cc0a29bb03c
Adam Day <adamday@daydevelops.com>
2019-03-16 13:17:28 -0230

    Added article image to header

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -10,55 +10,62 @@
 		<div class="header">
 			<i class="fas fa-user"></i>{{ $article->user->name ?? 'null' }}，
 			@if(count($article->tags))
-			<i class="fas fa-tags"></i>
+				<i class="fas fa-tags"></i>
 				@foreach($article->tags as $tag)
 					<a href="{{ url('tag', ['tag' => $tag->tag]) }}">{{ $tag->tag }}</a>，
 				@endforeach
 			@endif
 			<i class="fas fa-clock"></i>{{ $article->published_at->diffForHumans() }}
 		</div>
+
+
 	@endcomponent
 @endsection
 @section('content')
 
 
-    <div class="article container">
-        <div class="row">
-            <div class="col-md-8 offset-md-2">
+	<div class="article container">
+		<div class="row">
+			<div class="col-12 article-img-wrap">
+				<img src="{{$article->page_image}}" alt="Cover Image" id="article-img">
+			</div>
+		</div>
 
-            <parse content="{{ $article->content['raw'] }}"></parse>
-
-            @if($article->is_original)
-                <div class="publishing alert alert-dismissible alert-info">
-                    <button type="button" class="close" data-dismiss="alert">×</button>
-                    {!! config('blog.license') !!}
-                </div>
-                @endif
-                @if(config('blog.social_share.article_share'))
-                <div class="footing">
-                    <div class="social-share"
-                        data-title="{{ $article->title }}"
-                        data-description="{{ $article->title }}"
-                        {{ config('blog.social_share.sites') ? "data-sites=" . config('blog.social_share.sites') : '' }}
-                        {{ config('blog.social_share.mobile_sites') ? "data-mobile-sites=" . config('blog.social_share.mobile_sites') : '' }}
-                        initialized></div>
-                </div>
-                @endif
-            </div>
-        </div>
-    </div>
-    @if(Auth::guest())
-        <comment title="Comments"
-                 commentable-type="articles"
-                 commentable-id="{{ $article->id }}"></comment>
-    @else
-        <comment title="Comments"
-                 username="{{ Auth::user()->name }}"
-                 user-avatar="{{ Auth::user()->avatar }}"
-                 commentable-type="articles"
-                 commentable-id="{{ $article->id }}"
-                 can-comment></comment>
-    @endif
+		<div class="row">
+			<div class="col-md-10 offset-md-1">
+				<parse content="{{ $article->content['raw'] }}"></parse>
+				@if(false)
+					{{-- @if($article->is_original) --}}
+					<div class="publishing alert alert-dismissible alert-info">
+						<button type="button" class="close" data-dismiss="alert">×</button>
+						{!! config('blog.license') !!}
+					</div>
+				@endif
+				@if(config('blog.social_share.article_share'))
+					<div class="footing">
+						<div class="social-share"
+						data-title="{{ $article->title }}"
+						data-description="{{ $article->title }}"
+						{{ config('blog.social_share.sites') ? "data-sites=" . config('blog.social_share.sites') : '' }}
+						{{ config('blog.social_share.mobile_sites') ? "data-mobile-sites=" . config('blog.social_share.mobile_sites') : '' }}
+						initialized></div>
+					</div>
+				@endif
+			</div>
+		</div>
+	</div>
+	@if(Auth::guest())
+		<comment title="Comments"
+		commentable-type="articles"
+		commentable-id="{{ $article->id }}"></comment>
+	@else
+		<comment title="Comments"
+		username="{{ Auth::user()->name }}"
+		user-avatar="{{ Auth::user()->avatar }}"
+		commentable-type="articles"
+		commentable-id="{{ $article->id }}"
+		can-comment></comment>
+	@endif
 
 @endsection
 
@@ -71,7 +78,7 @@
 @endsection
 
 @section('scripts')
-    <script>
-        hljs.initHighlightingOnLoad();
-    </script>
+	<script>
+	hljs.initHighlightingOnLoad();
+	</script>
 @endsection
