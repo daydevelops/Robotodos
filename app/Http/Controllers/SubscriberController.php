6492938b104eef4357804f057253e9d7a040999b
Adam Day <adamday@daydevelops.com>
2019-03-17 17:47:12 -0230

    started setup for subscriptions

---
+++ app/Http/Controllers/SubscriberController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubscriberController extends Controller
+{
+    public function store() {
+		echo "hello";
+		return "hello";
+	}
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,11 +4,36 @@ namespace Tests;
 
 use App\User;
 use Laravel\Passport\Passport;
+use App\Exceptions\Handler;
+use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->disableExceptionHandling();
+	}
+	protected function disableExceptionHandling() {
+		$this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
+		$this->app->instance(ExceptionHandler::class, new class extends Handler {
+			public function __construct() {
+
+			}
+			public function report(\Exception $e) {
+
+			}
+			public function render($request, \Exception $e) {
+				throw $e;
+			}
+		});
+	}
+	protected function withExceptionHandling() {
+		$this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
+		return $this;
+	}
 
     public function actingAsAdmin()
     {
