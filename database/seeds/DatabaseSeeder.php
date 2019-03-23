83f5c8a99893da0b08c1f2bcc017465c4e3c646b
Adam Day <adamday@daydevelops.com>
2019-03-23 11:15:26 -0230

    set up seed for production

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,24 +1,41 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-         $this->call(UsersTableSeeder::class);
-         $this->call(CategoriesTableSeeder::class);
-         $this->call(ArticlesTableSeeder::class);
-         $this->call(DiscussionsTableSeeder::class);
-         $this->call(CommentsTableSeeder::class);
-         $this->call(TagsTableSeeder::class);
-         $this->call(LinksTableSeeder::class);
-         $this->call(VisitorsTableSeeder::class);
-         $this->call(PermissionTableSeeder::class);
-    }
+	/**
+	* Run the database seeds.
+	*
+	* @return void
+	*/
+	public function run()
+	{
+		if (app('env')=='production') {
+			$admin = [
+					'name' => 'AdamDay',
+					'email' => env('ADMIN_USER_EMAIL'),
+					'password' => Hash::make(env('ADMIN_USER_PASS')),
+					'status' => true,
+					'is_admin' => true,
+					'confirm_code' => str_random(64),
+					'created_at'  => Carbon::now(),
+					'updated_at'  => Carbon::now()
+
+			];
+			DB::table('users')->insert($admin);
+			$this->call(PermissionTableSeeder::class);
+		} else {
+			$this->call(UsersTableSeeder::class);
+			$this->call(CategoriesTableSeeder::class);
+			$this->call(ArticlesTableSeeder::class);
+			$this->call(DiscussionsTableSeeder::class);
+			$this->call(CommentsTableSeeder::class);
+			$this->call(TagsTableSeeder::class);
+			$this->call(LinksTableSeeder::class);
+			$this->call(VisitorsTableSeeder::class);
+			$this->call(PermissionTableSeeder::class);
+		}
+	}
 }
