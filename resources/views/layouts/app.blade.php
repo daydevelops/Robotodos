d6461b771e87404a2787f1119745e6a5497c8de5
Adam Day <adamday@daydevelops.com>
2019-03-25 17:21:53 -0230

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,20 @@
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-	<link rel="shortcut icon" href="{{ config('blog.default_icon') }}">
+
+
+	<link rel="apple-touch-icon" sizes="152x152" href="images/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
+	<link rel="manifest" href="images/site.webmanifest">
+	<link rel="mask-icon" href="images/safari-pinned-tab.svg" color="#5bbad5">
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="msapplication-config" content="images/browserconfig.xml">
+	<meta name="theme-color" content="#ffffff">
+
+
+	{{-- <link rel="shortcut icon" href="{{ config('blog.default_icon') }}"> --}}
 
 	<title>@yield('title', config('app.name'))</title>
 
