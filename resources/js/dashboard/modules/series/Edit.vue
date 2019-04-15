3080cc5b642202cd99c2552a49ede92fad69be0d
Adam Day <adamday@daydevelops.com>
2019-04-15 20:35:20 -0230

    added name update form to series dash page

--- resources/js/dashboard/modules/series/Edit.vue
+++ resources/js/dashboard/modules/series/Edit.vue
@@ -1,26 +1,43 @@
 <template>
-	<div>
-		<div id='articles-list'>
-			<draggable v-model="articles" @start="drag=true" @end="drag=false">
-				<div class='row' v-for="art in articles" :key="art.id">
+	<div id="edit-wrap">
+		<div class='text-center'>
+			<div class=" form-inline d-inline">
+				<label class="sr-only" for="series-name">Name</label>
+				<input type="text" class="form-control mb-2 mr-sm-2" id="series-name">
+
+				<button class="btn btn-primary mb-2" @click="updateName">Update Name</button>
+			</div>
+		</div>
+		<br><hr><br>
+		<div>
+			<div id='articles-list'>
+				<draggable v-model="articles" @start="drag=true" @end="drag=false">
+					<div class='row article-wrap' v-for="art in articles" :key="art.id">
+						<div class='col-10'>
+							<span class="article">{{art.title}}</span>
+						</div>
+						<div class='col-2 text-right'>
+							<button @click="deleteArticle(art.id)" class='btn btn-danger d-inline'>Remove</button>
+						</div>
+					</div>
+				</draggable>
+			</div>
+			<div id='new-article-wrap' v-if="articles_available.length>0">
+				<div class='row'>
 					<div class='col-10'>
-						<p class="article">{{art.title}}</p>
+						<select id='new-art-select' class="form-control form-control-lg">
+							<option class='add-art-option' value="0">None</option>
+							<option v-for="art in articles_available" :key="art.id" :id='"option-"+art.id' class="add-art-option" :value="art.id" v-text="art.title"></option>
+						</select>
 					</div>
 					<div class='col-2'>
-						<button @click="deleteArticle(art.id)" class='btn btn-danger'>Remove</button>
+						<button @click="addArticle()" class='btn btn-primary'>Add</button>
 					</div>
 				</div>
-			</draggable>
-		</div>
-		<div id='new-article-wrap' v-if="articles_available.length>0">
-			<select id='new-art-select' class="form-control form-control-lg">
-				<option class='add-art-option' value="0">None</option>
-				<option v-for="art in articles_available" :key="art.id" :id='"option-"+art.id' class="add-art-option" :value="art.id" v-text="art.title"></option>
-			</select>
-			<button @click="addArticle()" class='btn btn-primary'>Add</button>
-		</div>
-		<div>
-			<button class='btn btn-success' @click="updateSeries">update</button>
+			</div>
+			<div class='text-center'>
+				<button class='btn btn-success d-inline' @click="updateSeries">Update Articles</button>
+			</div>
 		</div>
 	</div>
 </template>
@@ -45,6 +62,7 @@ export default {
 			this.articles = this.series.articles;
 			// debugger
 			this.articles_available = response.data.articles_available;
+			$('#series-name').val(this.series.name);
 		})
 	},
 	methods: {
@@ -79,7 +97,13 @@ export default {
 			console.log()
 			this.$http.patch('series/order/' + this.$route.params.id, {articles:new_articles})
 			.then((response) => {
-				// location.reload();
+				toastr.success('Series updated!')
+			})
+		},
+		updateName() {
+			this.$http.patch('series/' + this.$route.params.id, {name:$('#series-name').val()})
+			.then((response) => {
+				toastr.success('Series updated!')
 			})
 		}
 
@@ -87,17 +111,22 @@ export default {
 }
 </script>
 <style>
-#articles-list {
+#articles-list, #new-article-wrap {
 	width:80%;
 	margin:auto;
 }
-#articles-list .article {
+#articles-list .article-wrap {
 	border:1px solid black;
-	margin:10px 0px;
+	background-color:white;
+	box-shadow:0px 0px 3px black;
+	margin:3px 0px;
 	width:100%;
-	padding:10px;
+	padding:5px;
 	font-size:18px;
-	border-radius:10px;
+	border-radius:5px;
+}
+#edit-wrap {
+	margin:20px 0px;
 }
 
 
